7568c07905348c801c5b72606fe55d00bf03a371
jonniebigodes <jonniebigodes@hotmail.com>
2018-09-18 20:37:40 +0100

    docs(babelrc and enzyme): corrected babelrc and linted enzymeconfig
    changed babelrc to allow production exports and transpiling and enzymeconfig was linted to stop
    annoying about some rules not being enforced

--- enzymeconfig.js
+++ enzymeconfig.js
@@ -1,5 +1,7 @@
+/* eslint-disable */
 import Enzyme,{shallow,render,mount} from 'enzyme';
 import Adapter from'enzyme-adapter-react-16';
+/* eslint-enable */
 
 Enzyme.configure({adapter:new Adapter()});
 // Make Enzyme functions available in all test files without importing
