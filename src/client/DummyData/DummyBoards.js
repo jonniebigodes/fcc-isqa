0aa376736da5e792b631559f3cde0afa1d68a9e4
jonniebigodes <jonniebigodes@hotmail.com>
2019-03-15 02:20:59 +0000

    fix(DummyBoard.js): forgot to add dummyboard.js
    adding dummyboard.js as its required

---
+++ src/client/DummyData/DummyBoards.js
@@ -0,0 +1,220 @@
+export const BoardsData = [
+  {
+    id: '5ba152a49d1b3401ac1a30d1',
+    title: 'testmessageboard',
+    creationdate: '2018-10-07T19:46:42.765Z',
+    threads: []
+  },
+  {
+    id: '5ba152ad4249f236dc3ad7a7',
+    title: 'mbtesting',
+    creationdate: '2018-10-07T19:46:42.765Z',
+    threads: [
+      {
+        created_on: '2018-09-18T19:31:55.828Z',
+        bumped_on: '2018-09-18T19:32:07.545Z',
+        reported: false,
+        _id: '5ba152ad4249f236dc3ad7a8',
+        thread_text: 'testthread',
+        board_id: '5ba152ad4249f236dc3ad7a7',
+        thread_delete_password: 'testdelpwd',
+        replies: [
+          {
+            _id: '5ba152b70d909a3cecde0550',
+            reply_text: '[deleted]',
+            dateadded: '2018-09-18T19:32:07.443Z',
+            reportedreply: true,
+            reply_delete_password: 'delete1'
+          },
+          {
+            _id: '5ba152b70d909a3cecde0551',
+            reply_text: '[deleted]',
+            dateadded: '2018-09-18T19:32:07.529Z',
+            reportedreply: true,
+            reply_delete_password: 'delete2'
+          },
+          {
+            _id: '5ba152b70d909a3cecde0552',
+            reply_text: 'testing reply 3',
+            dateadded: '2018-09-18T19:32:07.545Z',
+            reportedreply: false,
+            reply_delete_password: 'delete3'
+          }
+        ]
+      },
+      {
+        created_on: '2018-09-18T19:31:55.828Z',
+        bumped_on: '2018-09-18T19:32:07.601Z',
+        reported: false,
+        _id: '5ba152ad4249f236dc3ad7a9',
+        thread_text: 'testthread2',
+        board_id: '5ba152ad4249f236dc3ad7a7',
+        thread_delete_password: 'testdelpwd2',
+        replies: [
+          {
+            _id: '5ba152b70d909a3cecde0553',
+            reply_text: '[deleted]',
+            dateadded: '2018-09-18T19:32:07.565Z',
+            reportedreply: false,
+            reply_delete_password: 'delete4'
+          },
+          {
+            _id: '5ba152b70d909a3cecde0554',
+            reply_text: '[deleted]',
+            dateadded: '2018-09-18T19:32:07.584Z',
+            reportedreply: false,
+            reply_delete_password: 'delete5'
+          },
+          {
+            _id: '5ba152b70d909a3cecde0555',
+            reply_text: 'testing reply 6',
+            dateadded: '2018-09-18T19:32:07.601Z',
+            reportedreply: false,
+            reply_delete_password: 'delete6'
+          }
+        ]
+      },
+      {
+        created_on: '2018-09-18T19:31:55.828Z',
+        bumped_on: '2018-09-18T19:32:07.654Z',
+        reported: true,
+        _id: '5ba152ae4249f236dc3ad7aa',
+        thread_text: 'test1',
+        board_id: '5ba152ad4249f236dc3ad7a7',
+        thread_delete_password: 'test1',
+        replies: [
+          {
+            _id: '5ba152b70d909a3cecde0556',
+            reply_text: 'testing reply 7',
+            dateadded: '2018-09-18T19:32:07.637Z',
+            reportedreply: false,
+            reply_delete_password: 'delete7'
+          },
+          {
+            _id: '5ba152b70d909a3cecde0557',
+            reply_text: 'testing reply 8',
+            dateadded: '2018-09-18T19:32:07.654Z',
+            reportedreply: false,
+            reply_delete_password: 'delete8'
+          }
+        ]
+      },
+      {
+        created_on: '2018-09-18T19:31:55.828Z',
+        bumped_on: '2018-09-18T19:31:55.828Z',
+        reported: true,
+        _id: '5ba152ae4249f236dc3ad7ab',
+        thread_text: 'test2',
+        board_id: '5ba152ad4249f236dc3ad7a7',
+        thread_delete_password: 'test2',
+        replies: []
+      }
+    ]
+  },
+  {
+    id: '5ba152ad4249f236dc3ad7g7',
+    title: 'reportedTesting',
+    creationdate: '2018-10-07T19:46:42.765Z',
+    threads: [
+      {
+        created_on: '2018-09-18T19:31:55.828Z',
+        bumped_on: '2018-09-18T19:32:07.545Z',
+        reported: true,
+        _id: '5ba152ad4249f236dc3ad7a8',
+        thread_text: 'testthread',
+        board_id: '5ba152ad4249f236dc3ad7a7',
+        thread_delete_password: 'testdelpwd',
+        replies: [
+          {
+            _id: '5ba152b70d909a3cecde0550',
+            reply_text: '[deleted]',
+            dateadded: '2018-09-18T19:32:07.443Z',
+            reportedreply: false,
+            reply_delete_password: 'delete1'
+          },
+          {
+            _id: '5ba152b70d909a3cecde0551',
+            reply_text: '[deleted]',
+            dateadded: '2018-09-18T19:32:07.529Z',
+            reportedreply: false,
+            reply_delete_password: 'delete2'
+          },
+          {
+            _id: '5ba152b70d909a3cecde0552',
+            reply_text: 'testing reply 3',
+            dateadded: '2018-09-18T19:32:07.545Z',
+            reportedreply: false,
+            reply_delete_password: 'delete3'
+          }
+        ]
+      },
+      {
+        created_on: '2018-09-18T19:31:55.828Z',
+        bumped_on: '2018-09-18T19:32:07.601Z',
+        reported: false,
+        _id: '5ba152ad4249f236dc3ad7a9',
+        thread_text: 'testthread2',
+        board_id: '5ba152ad4249f236dc3ad7a7',
+        thread_delete_password: 'testdelpwd2',
+        replies: [
+          {
+            _id: '5ba152b70d909a3cecde0553',
+            reply_text: '[deleted]',
+            dateadded: '2018-09-18T19:32:07.565Z',
+            reportedreply: false,
+            reply_delete_password: 'delete4'
+          },
+          {
+            _id: '5ba152b70d909a3cecde0554',
+            reply_text: '[deleted]',
+            dateadded: '2018-09-18T19:32:07.584Z',
+            reportedreply: false,
+            reply_delete_password: 'delete5'
+          },
+          {
+            _id: '5ba152b70d909a3cecde0555',
+            reply_text: 'testing reply 6',
+            dateadded: '2018-09-18T19:32:07.601Z',
+            reportedreply: false,
+            reply_delete_password: 'delete6'
+          }
+        ]
+      },
+      {
+        created_on: '2018-09-18T19:31:55.828Z',
+        bumped_on: '2018-09-18T19:32:07.654Z',
+        reported: true,
+        _id: '5ba152ae4249f236dc3ad7aa',
+        thread_text: 'test1',
+        board_id: '5ba152ad4249f236dc3ad7a7',
+        thread_delete_password: 'test1',
+        replies: [
+          {
+            _id: '5ba152b70d909a3cecde0556',
+            reply_text: 'testing reply 7',
+            dateadded: '2018-09-18T19:32:07.637Z',
+            reportedreply: false,
+            reply_delete_password: 'delete7'
+          },
+          {
+            _id: '5ba152b70d909a3cecde0557',
+            reply_text: 'testing reply 8',
+            dateadded: '2018-09-18T19:32:07.654Z',
+            reportedreply: false,
+            reply_delete_password: 'delete8'
+          }
+        ]
+      },
+      {
+        created_on: '2018-09-18T19:31:55.828Z',
+        bumped_on: '2018-09-18T19:31:55.828Z',
+        reported: true,
+        _id: '5ba152ae4249f236dc3ad7ab',
+        thread_text: 'test2',
+        board_id: '5ba152ad4249f236dc3ad7a7',
+        thread_delete_password: 'test2',
+        replies: []
+      }
+    ]
+  }
+]
